a895d8068dfaca9013925e0dcb9583c1bae85af1
v.maruk <v.maruk@atomgroup.io>
2024-04-24 14:42:41 +0300

    v2

--- config/plugins.js
+++ config/plugins.js
@@ -15,7 +15,6 @@ module.exports = ({ env }) => ({
         "https://api.vercel.com/v1/integrations/deploy/prj_dHOOD4MEzMi26WejJYEgzziJeiLN/Ew5CuxrBoF",
       apiToken: "X6Dr59pGscgg3PEKvPPm4yz9",
       appFilter: "mota-cms",
-      teamFilter: "your-team-id-on-vercel",
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
   },
